34f3d3c99cd9f13f4acd2e75a24abb0027468efb
kareem <kareemhanysultan2004@gmail.com>
2026-06-11 18:20:19 +0300

    define post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,15 +1,17 @@
-    const router = require("express").Router();
-    const auth = require("../middleware/authMiddleware");
-    const {
-    createPost,
-    getPosts,
-    updatePost,
-    deletePost,
-    } = require("../controllers/postController");
+    // const router = require("express").Router();
+    // const auth = require("../middleware/authMiddleware");
+    // const {
+    // createPost,
+    // getPosts,
+    // updatePost,
+    // deletePost,
+    // } = require("../controllers/postController");
 
-    router.get("/", getPosts);
-    router.post("/", auth, createPost);
-    router.put("/:id", auth, updatePost);
-    router.delete("/:id", auth, deletePost);
+    // router.get("/", getPosts);
+    // router.post("/", auth, createPost);
+    // router.put("/:id", auth, updatePost);
+    // router.delete("/:id", auth, deletePost);
 
-    module.exports = router;
+    // module.exports = router;
+
+    
