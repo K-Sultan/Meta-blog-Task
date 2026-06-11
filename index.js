1894f10d84d286ae5c42d2cec8bead38c2623db8
kareem <kareemhanysultan2004@gmail.com>
2026-06-11 18:25:28 +0300

    fixing index.js

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ console.log("DB:", process.env.MONGODB_URI);
 const connectDB = require("./config/db")
 
 
+// const app = express()
+// connectDB();
+
+// app.use(express.json())
+
+// //routes
+// app.use("/auth", require("./routes/authRoutes"));
+// app.use("/posts", require("./routes/postRoutes"));
+
+
 const app = express()
 connectDB();
-
 app.use(express.json())
 
 //routes
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -14,4 +14,13 @@
 
     // module.exports = router;
 
-    
+const router = require("express").Router();
+const auth = require("../middleware/authMiddleware");
+const { createPost, getPosts, updatePost, deletePost } = require("../controllers/postController");
+
+router.get("/", getPosts);
+router.post("/", auth, createPost);
+router.put("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+
+module.exports = router;
