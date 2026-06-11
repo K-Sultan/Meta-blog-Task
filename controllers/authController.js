4aef4978b922f9c914916f244d68ab5447bdddf1
kareem <kareemhanysultan2004@gmail.com>
2026-06-11 18:15:35 +0300

    the auth controller logic for register

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,3 +77,28 @@
         res.status(500).json({ message: err.message });
     }
     };
+
+    // LOGIN
+    exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user)
+        return res.status(401).json({ message: "Invalid credentials" });    
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+        return res.status(401).json({ message: "Invalid credentials" });
+
+        const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+        );
+
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+    };  
