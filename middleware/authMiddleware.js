9e8f856e1ecca96fc6b133e5752adc4fc03712c6
kareem <kareemhanysultan2004@gmail.com>
2026-06-11 18:16:21 +0300

    the auth controller logic for register

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,25 @@
-    const jwt = require("jsonwebtoken");
+    // const jwt = require("jsonwebtoken");
 
-    module.exports = (req, res, next) => {
-    const token = req.header("Authorization");
+    // module.exports = (req, res, next) => {
+    // const token = req.header("Authorization");
 
-    if (!token)
-        return res.status(401).json({ message: "No token, access denied" });
+    // if (!token)
+    //     return res.status(401).json({ message: "No token, access denied" });
 
+    // try {
+    //     const verified = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
+    //     req.user = verified;
+    //     next();
+    // } catch (err) {
+    //     res.status(401).json({ message: "Invalid token" });
+    // }
+    // };
+
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const token = req.header("Authorization");    
+    if (!token) return res.status(401).json({ message: "No token, access denied" });
     try {
         const verified = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
         req.user = verified;
@@ -13,4 +27,4 @@
     } catch (err) {
         res.status(401).json({ message: "Invalid token" });
     }
-    };
+};  
