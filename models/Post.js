a4d22deaddd9b3336660a1b4958795b91c0c6dc3
kareem <kareemhanysultan2004@gmail.com>
2026-06-11 18:17:53 +0300

    schema for Post

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,34 @@
-    const mongoose = require("mongoose");
+    // const mongoose = require("mongoose");
 
-    const postSchema = new mongoose.Schema({
+    // const postSchema = new mongoose.Schema({
+    // title: {
+    //     type: String,
+    //     required: true,
+    //     trim: true
+    // },
+
+    // content: {
+    //     type: String,
+    //     required: true
+    // },
+
+    // authorId: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "User",
+    //     required: true
+    // },
+
+    // createdAt: {
+    //     type: Date,
+    //     default: Date.now
+    // }
+    // });
+
+    // module.exports = mongoose.model("Post", postSchema);
+
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({    
     title: {
         type: String,
         required: true,
