b9c5129d3cc9c228e16dfa6f492433c09725a39a
kareem <kareemhanysultan2004@gmail.com>
2026-06-11 18:18:41 +0300

    schema for User

--- models/User.js
+++ models/User.js
@@ -1,6 +1,34 @@
-    const mongoose = require("mongoose");
+    // const mongoose = require("mongoose");
 
-    const userSchema = new mongoose.Schema({
+    // const userSchema = new mongoose.Schema({
+    // name: {
+    //     type: String,
+    //     required: true,
+    //     trim: true
+    // },
+
+    // email: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
+
+    // createdAt: {
+    //     type: Date,
+    //     default: Date.now
+    // }
+    // });
+
+    // module.exports = mongoose.model("User", userSchema);
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
